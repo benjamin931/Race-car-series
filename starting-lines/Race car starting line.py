65fcdbb21464295bf010b772a5367fbad95235aa
Ben <benjamindavidbook@gmail.com>
2025-02-23 14:41:34 -0600

    Update Race car starting line.py

--- starting-lines/Race car starting line.py
+++ starting-lines/Race car starting line.py
@@ -1,17 +1,17 @@
 # This script causes an external LED to glow when a button is pressed.
 
 from machine import Pin #By loading only the class called "Pin", we don't have to use machine.Pin in our code, we just use Pin
-import utime
+from utime import sleep
 
 # Define GPIO pins
-button_pin = 16  # Button connected to GPIO 16
-led1_pin = 15     # Connection for external LED
-led2_pin = 14
-led3_pin = 13
-led4_pin = 12
+button_pin = 6  # Button connected to GPIO 6
+led1_pin = 0     # Connection for external LEDs
+led2_pin = 1
+led3_pin = 2
+led4_pin = 3
 
-# Set up the button as an input with an external pull-up resistor
-button = Pin(button_pin, Pin.IN, None)
+# Set up the button as an input with an internal pull-up resistor
+button = Pin(button_pin, Pin.IN, Pin.PULL_DOWN)
 
 # Set up the LED as an output
 led1 = Pin(led1_pin, Pin.OUT)
@@ -23,26 +23,19 @@ while True: #Using "while" in this way allows the program to run in a loop, unti
     # Check if the button is pressed (active high due to pull-down resistor)
     if button.value() == 1:
        led1.on()          #Turn the LED on
-       utime.sleep(1.2)     #Wait 1.2 seconds
+       sleep(1.2)     #Wait 1.2 seconds
        led1.off()         #Turn the LED off
-       utime.sleep(0.01)  #Wait 0.01 seconds
        led2.on()          #Turn the LED on
-       utime.sleep(1.2)     #Wait 1.2 seconds
+       sleep(1.2)     #Wait 1.2 seconds
        led2.off()         #Turn the LED off
-       utime.sleep(0.01)  #Wait 0.01 seconds
        led3.on()          #Turn the LED on
-       utime.sleep(1.2)     #Wait 1.2 seconds
+       sleep(1.2)     #Wait 1.2 seconds
        led3.off()         #Turn the LED off
-       utime.sleep(0.01)  #Wait 0.01 seconds
        led4.on()          #Turn the LED on
-       utime.sleep(2)     #Wait 2 seconds 
+       sleep(2)     #Wait 2 seconds 
        led4.off()         #Turn the LED off
-       utime.sleep(3)     #Wait 3 seconds
-    else:
-        led1.off()  # Turn off the LED when button is released
-        led2.off()
-        led3.off()
-        led4.off()
+    else:     #If button value is not = 1:
+        pass              #Do nothing
     
     # Small delay to debounce
     utime.sleep(0.01)
